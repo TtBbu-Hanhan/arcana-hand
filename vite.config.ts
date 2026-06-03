bb0b337a231f96c9a7eb5689ce6d44c67bb4105f
TtBbu <ttbbu@TtBbudeMacBook-Air.local>
2026-06-03 23:44:31 +0800

    chore: re-deploy after moving folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/arcana-hand/', // 👈 核心：在这里加上这行，注意后面有逗号
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
